7ebdc8595fac53b6a66ec59408aa3ea9549e185e
Alastair Hughes <hobbitalastair@gmail.com>
2019-10-19 09:49:17 +1300

    Add mkinitcpio.conf changes to workaround systemd 243 breaking modesetting

--- vostro1510/install.sh
+++ vostro1510/install.sh
@@ -3,8 +3,10 @@ post_install() {
     # wpa_supplicant is installed readable by other by default...
     chmod o-r /etc/wpa_supplicant/wpa_supplicant.conf
     patchman.sh -r /etc/systemd/logind.conf
+    patchman.sh -r /etc/mkinitcpio.conf
 }
 
 post_upgrade() {
     patchman.sh -r /etc/systemd/logind.conf
+    patchman.sh -r /etc/mkinitcpio.conf
 }
